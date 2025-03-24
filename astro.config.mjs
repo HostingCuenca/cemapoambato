d525c47459bdd7fa6a6d2be2301bd00608890e4a
HostingCuenca <42749056+roggrt@users.noreply.github.com>
2025-03-24 13:52:40 -0500

    prueba y error

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,21 +1,9 @@
-// // @ts-check
-// import { defineConfig } from 'astro/config';
-//
-// import tailwind from '@astrojs/tailwind';
-//
-// // https://astro.build/config
-// export default defineConfig({
-//   integrations: [tailwind()]
-// });
-
 // @ts-check
 import { defineConfig } from 'astro/config';
+
 import tailwind from '@astrojs/tailwind';
-import vercel from '@astrojs/vercel/serverless';
 
 // https://astro.build/config
 export default defineConfig({
-    output: 'hybrid',
-    adapter: vercel(),
-    integrations: [tailwind()]
+  integrations: [tailwind()]
 });
