1e6b01839f6f2e816b33e8a3470a7770c24e6f6a
roggrt <42749056+roggrt@users.noreply.github.com>
2025-03-25 18:09:27 -0500

    intentamos jaja

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,11 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
-
 import tailwind from '@astrojs/tailwind';
+import vercel from '@astrojs/vercel/serverless';
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind()],
-  output: 'hybrid', // Changed from default 'static' to 'hybrid'
+  output: 'hybrid',
+  adapter: vercel(),
 });
