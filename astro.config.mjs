7fd88f03bf0e5da324095516328e36562dfa2871
roggrt <42749056+roggrt@users.noreply.github.com>
2025-03-25 18:45:39 -0500

    volvimos al modo static jaja

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,7 @@
 import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
-import vercel from '@astrojs/vercel';
 
 export default defineConfig({
   integrations: [tailwind()],
-  output: 'server',
-  adapter: vercel(),
-  image: {
-    service: undefined, // ✅ Forma correcta de deshabilitar imágenes en Astro
-  },
-  
+  output: 'static'
 });
