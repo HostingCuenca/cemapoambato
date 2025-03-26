cc47bcf9f83d52c8742d0f8102340c6b8ab72e6f
HostingCuenca <42749056+roggrt@users.noreply.github.com>
2025-03-26 08:13:57 -0500

    cambios

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,22 @@
-// astro.config.mjs
+// // astro.config.mjs
+// import { defineConfig } from 'astro/config';
+// import tailwind from '@astrojs/tailwind';
+// import vercel from '@astrojs/vercel/serverless';
+//
+// export default defineConfig({
+//   integrations: [tailwind()],
+//   output: 'server',
+//   adapter: vercel()
+// });
+
+
 import { defineConfig } from 'astro/config';
+// Import /serverless for a Serverless SSR site
 import tailwind from '@astrojs/tailwind';
-import vercel from '@astrojs/vercel/serverless';
+import vercelServerless from '@astrojs/vercel/serverless';
 
 export default defineConfig({
   integrations: [tailwind()],
   output: 'server',
-  adapter: vercel()
+  adapter: vercelServerless(),
 });
